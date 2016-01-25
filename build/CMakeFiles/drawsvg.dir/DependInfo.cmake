473ae3ef8f10ca2cf4a6eb35c05bc4184eb2efcf
hilllo <hilllocanton@gmail.com>
2016-01-25 17:14:50 -0500

    upload original build

---
+++ build/CMakeFiles/drawsvg.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hilllo/GitHub/asst1_drawsvg/src/drawsvg.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/drawsvg.cpp.o"
+  "/home/hilllo/GitHub/asst1_drawsvg/src/main.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/main.cpp.o"
+  "/home/hilllo/GitHub/asst1_drawsvg/src/png.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/png.cpp.o"
+  "/home/hilllo/GitHub/asst1_drawsvg/src/software_renderer.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/software_renderer.cpp.o"
+  "/home/hilllo/GitHub/asst1_drawsvg/src/svg.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/svg.cpp.o"
+  "/home/hilllo/GitHub/asst1_drawsvg/src/texture.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/texture.cpp.o"
+  "/home/hilllo/GitHub/asst1_drawsvg/src/triangulation.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/triangulation.cpp.o"
+  "/home/hilllo/GitHub/asst1_drawsvg/src/viewport.cpp" "/home/hilllo/GitHub/asst1_drawsvg/build/CMakeFiles/drawsvg.dir/viewport.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/freetype2"
+  "/home/hilllo/GitHub/asst1_drawsvg/include"
+  "/home/hilllo/GitHub/asst1_drawsvg/include/GLEW"
+  "/home/hilllo/GitHub/asst1_drawsvg/include/GLFW"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
